dcba751fcac6b5f590324d8f9a14f08d2a309584
Elisio Leonardo <elisio.leonardo@gmail.com>
2012-10-22 03:20:19 +0200

    Funcao para Listar funcionarios semelhante ao find(list) mas usando name da Entidade

--- app/Model/Funcionario.php
+++ app/Model/Funcionario.php
@@ -1,165 +1,173 @@
 <?php
+
 /**
  * Model do Funcionario
- * 
+ *
  * @copyright     Copyright 2010-2011, INFOmoz (Inform�tica-Mo�ambique) (http://infomoz.net)
- ** @link          http://opensga.com OpenSGA  - Sistema de Gestão Académica
+ * * @link          http://opensga.com OpenSGA  - Sistema de Gestão Académica
  * @author		  Elisio Leonardo (elisio.leonardo@gmail.com)
  * @package       opensga
  * @subpackage    opensga.core.controller
  * @since         OpenSGA v 0.10.0.0
 
- * 
+ *
  */
- 
- 
 class Funcionario extends AppModel {
-	var $name = 'Funcionario';
-    
+
+    var $name = 'Funcionario';
     public $actsAs = array('Auditable.Auditable');
-    
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
-	var $belongsTo = array(
-		'User' => array(
-			'className' => 'User',
-			'foreignKey' => 'user_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
-		'GrauAcademico' => array(
-			'className' => 'GrauAcademico',
-			'foreignKey' => 'grau_academico_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
-		'Cargo' => array(
-			'className' => 'Cargo',
-			'foreignKey' => 'cargo_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
-		'Departamento' => array(
-			'className' => 'Departamento',
-			'foreignKey' => 'departamento_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
-		'Tipofuncionario' => array(
-			'className' => 'Tipofuncionario',
-			'foreignKey' => 'tipofuncionario_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
-		'Entidade' => array(
-			'className' => 'Entidade',
-			'foreignKey' => 'entidade_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
-		'UnidadeOrganica' => array(
-			'className' => 'UnidadeOrganica',
-			'foreignKey' => 'unidade_organica_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
-		'Seccao' => array(
-			'className' => 'Seccao',
-			'foreignKey' => 'seccao_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
-		'Faculdade' => array(
-			'className' => 'Faculdade',
-			'foreignKey' => 'faculdade_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		),
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
+    var $belongsTo = array(
+        'User' => array(
+            'className' => 'User',
+            'foreignKey' => 'user_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'GrauAcademico' => array(
+            'className' => 'GrauAcademico',
+            'foreignKey' => 'grau_academico_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'Cargo' => array(
+            'className' => 'Cargo',
+            'foreignKey' => 'cargo_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'Departamento' => array(
+            'className' => 'Departamento',
+            'foreignKey' => 'departamento_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'Tipofuncionario' => array(
+            'className' => 'Tipofuncionario',
+            'foreignKey' => 'tipofuncionario_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'Entidade' => array(
+            'className' => 'Entidade',
+            'foreignKey' => 'entidade_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'UnidadeOrganica' => array(
+            'className' => 'UnidadeOrganica',
+            'foreignKey' => 'unidade_organica_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'Seccao' => array(
+            'className' => 'Seccao',
+            'foreignKey' => 'seccao_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'Faculdade' => array(
+            'className' => 'Faculdade',
+            'foreignKey' => 'faculdade_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
         'SuperiorHieraquico' => array(
-			'className' => 'Funcionario',
-			'foreignKey' => 'superior_hierarquico',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-        
-		
-	);
+            'className' => 'Funcionario',
+            'foreignKey' => 'superior_hierarquico',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
 
+    /**
+     *Esta funcao faz o mesmo que find list, mas busca o name a partir da tabela entidades
+     */
+    public function listaFuncionarios(){
 
-        function geraCodigo(){
-            $id = $this->find('first', array('order' => array('Funcionario.created DESC'),'fields'=>'id'));
-            $ano=date('Y');
-            $id_for=str_pad($id['Funcionario']['id']+1, 5,"0",STR_PAD_LEFT);
-            $codigo = $ano.$id_for;
-            return $codigo;
-        }
-        
-        /**
-         *
-         * @param type $name
-         * @return string 
-         * 
-         * @deprecated Usa o geraUsername dos users que eh global
-         */
-        function criaUsername($name){
-            $nome_lw = strtolower($name);
-            $nome_ex = explode(' ',$nome_lw);
-            $nome_1 = $nome_ex[0];
-            $nome_2 = end($nome_ex);
-            $nome = $nome_1.".".$nome_2;
-            $numero = $this->User->find('count',array('conditions'=>array('username'=>$nome)));
-            if($numero>0){
-                //$numero=$numero+1;
-                $nome = $nome.$numero;
-            }
-            
-            return $nome;
-        }
-		
+        $funcionarios = $this->find('all',array('fields'=>array('id','Entidade.name')));
 
-		function getTurmasByFuncionario($funcionario_id){
-            $query = "SELECT tt.id FROM t0010turmas tt, funcionarios tf where (tf.id = tt.funcionario_id or tt.funcionario_ass_id = tf.id) and tf.id  = {$funcionario_id} ";
-           	$resultado = $this->query($query);
-			return $resultado;
+        $f=array();
+        foreach($funcionarios as $funcionario){
+
+            $f[$funcionario[$this->alias]['id']]=$funcionario['Entidade']['name'];
         }
-        
-        public function cadastraFuncionario(array $data){
-            $dataSource = $this->getDataSource();
-            
-            $dataSource->begin();
-            
-            $data['User']['name']=$data['Entidade']['name'];
-            $data['User']['username'] = $this->User->geraUsername($data['Entidade']['name']);
-            $data['User']['codigo'] = $this->geraCodigo();
-            $data['User']['password']=AuthComponent::password('12345');
-            $data['User']['group_id'] = 2;
-            
-            $this->User->create();
-            if($this->User->save($data)){
-                $data['Entidade']['user_id'] = $this->User->id;
-                $this->Entidade->create();
-                if($this->Entidade->save($data)){
-                    $data['Funcionario']['user_id'] = $this->User->id;
-                    $data['Funcionario']['entidade_id'] = $this->Entidade->id;
-                    $this->create();
-                    if($this->save($data)){
-                        return $dataSource->commit();
-                    }
+        return $f;
+    }
+
+    function geraCodigo() {
+        $id = $this->find('first', array('order' => array('Funcionario.created DESC'), 'fields' => 'id'));
+        $ano = date('Y');
+        $id_for = str_pad($id['Funcionario']['id'] + 1, 5, "0", STR_PAD_LEFT);
+        $codigo = $ano . $id_for;
+        return $codigo;
+    }
+
+    /**
+     *
+     * @param type $name
+     * @return string
+     *
+     * @deprecated Usa o geraUsername dos users que eh global
+     */
+    function criaUsername($name) {
+        $nome_lw = strtolower($name);
+        $nome_ex = explode(' ', $nome_lw);
+        $nome_1 = $nome_ex[0];
+        $nome_2 = end($nome_ex);
+        $nome = $nome_1 . "." . $nome_2;
+        $numero = $this->User->find('count', array('conditions' => array('username' => $nome)));
+        if ($numero > 0) {
+            //$numero=$numero+1;
+            $nome = $nome . $numero;
+        }
+
+        return $nome;
+    }
+
+    function getTurmasByFuncionario($funcionario_id) {
+        $query = "SELECT tt.id FROM t0010turmas tt, funcionarios tf where (tf.id = tt.funcionario_id or tt.funcionario_ass_id = tf.id) and tf.id  = {$funcionario_id} ";
+        $resultado = $this->query($query);
+        return $resultado;
+    }
+
+    public function cadastraFuncionario(array $data) {
+        $dataSource = $this->getDataSource();
+
+        $dataSource->begin();
+
+        $data['User']['name'] = $data['Entidade']['name'];
+        $data['User']['username'] = $this->User->geraUsername($data['Entidade']['name']);
+        $data['User']['codigo'] = $this->geraCodigo();
+        $data['User']['password'] = AuthComponent::password('12345');
+        $data['User']['group_id'] = 2;
+
+        $this->User->create();
+        if ($this->User->save($data)) {
+            $data['Entidade']['user_id'] = $this->User->id;
+            $this->Entidade->create();
+            if ($this->Entidade->save($data)) {
+                $data['Funcionario']['user_id'] = $this->User->id;
+                $data['Funcionario']['entidade_id'] = $this->Entidade->id;
+                $this->create();
+                if ($this->save($data)) {
+                    return $dataSource->commit();
                 }
             }
-              $dataSource->rollback();  
         }
-
-
+        $dataSource->rollback();
+    }
 
 }
+
 ?>
