c6aec624d08c9abe9571bb19d2cc0ab75d6d5d37
Elisio Leonardo <elisio.leonardo@gmail.com>
2012-10-24 23:10:31 +0200

    Novos modelos para gravar contactos das entidades

---
+++ app/Test/Case/Controller/EntidadeContactosControllerTest.php
@@ -0,0 +1,118 @@
+<?php
+App::uses('EntidadeContactosController', 'Controller');
+
+/**
+ * EntidadeContactosController Test Case
+ *
+ */
+class EntidadeContactosControllerTest extends ControllerTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.entidade_contacto',
+		'app.entidade',
+		'app.user',
+		'app.group',
+		'app.funcionario',
+		'app.grau_academico',
+		'app.cargo',
+		'app.tipofuncionario',
+		'app.unidade_organica',
+		'app.tipo_unidade_organica',
+		'app.area_academica',
+		'app.area_unidade',
+		'app.estado_objecto',
+		'app.docente',
+		'app.docente_categoria',
+		'app.aluno',
+		'app.curso',
+		'app.tipocurso',
+		'app.planoestudo',
+		'app.planoestudoano',
+		'app.disciplina',
+		'app.grupodisciplina',
+		'app.turma',
+		'app.anolectivo',
+		'app.regimelectivo',
+		'app.semestrelectivo',
+		'app.turno',
+		'app.estadoturma',
+		'app.inscricao',
+		'app.estadoinscricao',
+		'app.epocaavaliacao',
+		'app.matricula',
+		'app.estadomatricula',
+		'app.avaliacao',
+		'app.tipoavaliacao',
+		'app.turmatipoavaliacao',
+		'app.areatrabalho',
+		'app.aluno_nivel_medio',
+		'app.escola_nivel_medio',
+		'app.cidade',
+		'app.provincia',
+		'app.paise',
+		'app.financeiro_pagamento',
+		'app.financeiro_conta',
+		'app.financeiro_deposito',
+		'app.financeiro_estado_deposito',
+		'app.financeiro_forma_deposito',
+		'app.financeiro_banco',
+		'app.financeiro_tipo_pagamento',
+		'app.month',
+		'app.feriado',
+		'app.mensalidade',
+		'app.financeiro_estado_pagamento',
+		'app.genero',
+		'app.pais',
+		'app.documento_identificacao',
+		'app.estadoentidade',
+		'app.estado_civil',
+		'app.entidade_identificacao',
+		'app.tipo_contacto'
+	);
+
+/**
+ * testIndex method
+ *
+ * @return void
+ */
+	public function testIndex() {
+	}
+
+/**
+ * testView method
+ *
+ * @return void
+ */
+	public function testView() {
+	}
+
+/**
+ * testAdd method
+ *
+ * @return void
+ */
+	public function testAdd() {
+	}
+
+/**
+ * testEdit method
+ *
+ * @return void
+ */
+	public function testEdit() {
+	}
+
+/**
+ * testDelete method
+ *
+ * @return void
+ */
+	public function testDelete() {
+	}
+
+}
