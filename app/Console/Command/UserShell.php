6121526aae56919f6b20f3e0a1a4627f631ce22c
Elisio Leonardo <elisio.leonardo@gmail.com>
2014-07-29 21:13:34 +0200

    Nova Função para alterar Password da Linha de comando

--- app/Console/Command/UserShell.php
+++ app/Console/Command/UserShell.php
@@ -15,4 +15,52 @@ class UserShell extends AppShell {
 
 	}
 
+    /**
+     * Altera Password a partir da linha de comandos
+     * @param --userId
+     * @param --username
+     * @param --novaPassword  Se nao for passada, uma senha aleatoria é gerada
+     */
+    public function changePassword()
+    {
+        AuditableConfig::$Logger = ClassRegistry::init('Auditable.Logger');
+        $parametros = $this->params;
+        $conditions = array();
+        if (isset($parametros['username'])) {
+            $conditions['User.username'] = $parametros['username'];
+        } elseif (isset($parametros['userId'])) {
+            $conditions['User.id'] = $parametros['userId'];
+        }
+        if (empty($conditions)) {
+            $this->out('<error>Nenhuma condicao foi fonecida</error>');
+        } else {
+            $user = $this->User->find('first', array('conditions' => $conditions));
+            if (empty($user)) {
+                $this->out('<error>Usuario nao encontrado</error>');
+            } else {
+                if (isset($this->params['novaPassword'])) {
+                    $novaPassword = $this->User->resetPassword($user['User']['id'], $this->params['novaPassword']);
+                } else {
+                    $novaPassword = $this->User->resetPassword($user['User']['id']);
+                }
+
+                $this->out('<info>A sua senha foi alterada com sucesso</info>');
+                $this->out('<comment>Sua nova Password:' . $novaPassword . '</comment>');
+            }
+        }
+    }
+
+
+    public function getOptionParser()
+    {
+        $parser = parent::getOptionParser();
+        $parser->addOption('userId');
+        $parser->addOption('username');
+        $parser->addOption('novaPassword');
+
+        return $parser;
+
+
+    }
+
 }
