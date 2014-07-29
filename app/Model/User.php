05d843bc77eeeda234b78165958e554c26d5ca9e
Elisio Leonardo <elisio.leonardo@gmail.com>
2014-07-29 21:22:21 +0200

    Agora a função generatePassword aceita configurar a força da senha

--- app/Model/User.php
+++ app/Model/User.php
@@ -311,9 +311,16 @@ class User extends AppModel {
 		}
 	}
 
-	public function generatePassword($length = 8) {
-		$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
-		$password = substr(str_shuffle($chars), 0, $length);
+    public function generatePassword($length = 8, $capital = true, $symbol = true)
+    {
+        $chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+        if ($capital == true) {
+            $chars .= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        if ($symbol == true) {
+            $chars .= "!@#$%^&*()_-=+;:,.?";
+        }
+        $password = substr(str_shuffle($chars), 0, $length);
 		return $password;
 	}
 
