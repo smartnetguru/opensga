dba83502e5a96a55c1afd49cc96b230f86b8b548
Elisio Leonardo <elisio.leonardo@gmail.com>
2014-08-15 08:43:17 +0200

    SMS alumni

--- app/Controller/SmsNotificationsController.php
+++ app/Controller/SmsNotificationsController.php
@@ -101,22 +101,31 @@ class SmsNotificationsController extends AppController {
 				)
 			)
 		));
-		$alunos = $this->Aluno->find('all', array('conditions' => array('Aluno.codigo' => array(20095401, 20124663, 20140016, 20140167, 20140183, 20140183, 20140266, 20140288, 20130232, 20130337, 20140372, 20130336, 20140451, 20140488, 20140398, 20140534, 20140848, 20130869, 20130896, 20140939, 20130955, 20131012, 2013089, 20131018, 20141000, 20141137, 20144970, 20141193, 20123751, 20141375, 20141376, 20131320, 20121905, 20141377, 20141526, 20131365, 20131253, 20131366, 20141495, 20131358, 20141614, 20141591, 20141765, 20141762, 20141736, 20131564, 20131568, 20131652, 20141447, 20144615, 20141825, 20141823, 20141897, 20131745, 20141912, 20131750, 20141946, 20141947, 20142021, 20141994, 20142049, 20142249, 20132196, 20132214, 20132220, 20132156, 20132184, 20132195, 20124030, 20132175, 20142656, 20142602, 20144975, 20142749, 20142767, 20142879, 20143170, 20132968, 20143266, 20133040, 20143362, 20143352, 20143356, 20143477, 20143678, 20143664, 20143669, 20143666, 20143675, 20143690, 20143713, 20143639, 20124595, 20143783, 20143768, 20143759, 20143775, 20133505, 20133506, 20133502, 20133573, 20133733, 20133736))));
-		foreach ($alunos as $aluno) {
-			if (isset($aluno['Entidade']['EntidadeContacto'][0]['valor'])) {
-				$numero = $aluno['Entidade']['EntidadeContacto'][0]['valor'];
-				if (is_numeric($numero)) {
-					print_r($numero);
-					$mensagem = "Para se proceder o pagamento da sua bolsa, favor contactar a Direcção do Registo Académico–Reitoria para abertura de conta bancária ate dia 25/07";
+        $candidatos = $this->Aluno->CandidatoGraduacao->find('all');
 
-					//$this->SmsEnviada->sendSMS($numero, $mensagem);
+		foreach ($candidatos as $candidato) {
+			if ($candidato['CandidatoGraduacao']['telemovel']!='') {
+				$numero = $candidato['CandidatoGraduacao']['telemovel'];
+				if (is_numeric($numero)) {
+					debug($numero);
+
+					$mensagem = "A UEM, o convida a cerimónia de lançamento da iniciativa Alumni no dia 22/08/2014, no Centro Cultural e Universitário,
+pelas 14.00 horas.";
+
+					$this->SmsEnviada->sendSMS($numero, $mensagem);
 				}
+
+
+
 			}
 
 
 
 		}
-        //$this->SmsEnviada->sendSMS(826489374, "Todas SMS foram enviadas.");
+        $this->SmsEnviada->sendSMS(826489374, "Todas SMS foram enviadas.");
+        $this->SmsEnviada->sendSMS(844213948, "A UEM, o convida a cerimónia de lançamento da iniciativa Alumni no dia
+        22/08/2014, no Centro Cultural e Universitário,
+pelas 14.00 horas.");
 	}
 
 	public function testa_sms() {
