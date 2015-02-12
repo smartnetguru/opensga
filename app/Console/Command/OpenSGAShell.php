d6efca075b62d4a952ab3d0379a93ac7bc269c0c
Elisio Leonardo <elisio.leonardo@gmail.com>
2015-02-12 09:34:02 +0200

    12

--- app/Console/Command/OpenSGAShell.php
+++ app/Console/Command/OpenSGAShell.php
@@ -1128,7 +1128,7 @@
             if (!class_exists('PHPExcel'))
                 throw new CakeException('Vendor class PHPExcel not found!');
 
-            $xls = PHPExcel_IOFactory::load(APP . 'Imports' . DS .'Admitidos'.DS.'2015'.DS. 'admitidos_20150211.xlsx');
+            $xls = PHPExcel_IOFactory::load(APP . 'Imports' . DS .'Admitidos'.DS.'2015'.DS. 'admitidos_20150212.xlsx');
 
             $worksheet = $xls->getActiveSheet();
             //debug($xls->getActiveSheetIndex());
