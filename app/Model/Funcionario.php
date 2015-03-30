ecca52c25597993447a50ab969aa80073aa95deb
Elisio Leonardo <elisio.leonardo@gmail.com>
2015-03-30 11:06:23 +0200

    Funcionarios cadastro

--- app/Model/Funcionario.php
+++ app/Model/Funcionario.php
@@ -135,6 +135,7 @@ class Funcionario extends AppModel {
 		if ($this->User->save($data)) {
             //die(debug($this->User->id));
 			$data['Entidade']['user_id'] = $this->User->id;
+            $data['Entidade']['name'] = $data['Entidade']['nomes']." ".$data['Entidade']['apelido'];
 			$this->Entidade->create();
 			if ($this->Entidade->save($data)) {
 				$data['Funcionario']['user_id'] = $this->User->id;
