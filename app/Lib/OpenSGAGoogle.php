ed2ba6ccebaeb3a5c1b6014f3bb6ede7cb58da1c
Elisio Leonardo <elisio.leonardo@gmail.com>
2016-01-16 15:39:35 +0200

    Senhas removidas dos files

---
+++ app/Lib/OpenSGAGoogle.php
@@ -0,0 +1,166 @@
+<?php
+
+    class OpenSGAGoogle{
+
+
+        public function cria_conta($username,$firstName,$lastName,$password) {
+            Zend_Loader::loadClass('Zend_Gdata');
+            Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+            Zend_Loader::loadClass('Zend_Gdata_Gapps');
+
+            $service = Zend_Gdata_Gapps::AUTH_SERVICE_NAME;
+            $client = Zend_Gdata_ClientLogin::getHttpClient('elisio.leonardo@uem.ac.mz', 'COLOCAR SENHA AQUI', $service);
+            $gdata = new Zend_Gdata_Gapps($client, 'uem.ac.mz');
+
+            $this->Entidade->contain(array('User', 'Aluno'));
+            $users = $this->Entidade->find('all', array('conditions' => array('User.codigo_activacao' => NULL), 'order' => 'User.id DESC'));
+            var_dump(count($users));
+            $ii = 0;
+            foreach ($users as $u) {
+
+                $usernames = explode('@', $u['User']['username']);
+                $usernames[0] = str_replace(' ', '', $usernames[0]);
+                $mudar_apelido = false;
+                if ($u['Entidade']['apelido'] == null) {
+                    $apelidos = explode(' ', trim($u['Entidade']['name']));
+                    $mudar_apelido = true;
+                    $u['Entidade']['apelido'] = end($apelidos);
+                }
+
+                try {
+                    debug($u);
+                    $usuario = $gdata->createUser($usernames[0], $u['Entidade']['nomes'], $u['Entidade']['apelido'], 'dra02062013');
+                } catch (Zend_Gdata_Gapps_ServiceException $ex) {
+                    $codigo = $ex->getErrors();
+                    if (key($codigo) == 1300) {
+                        $this->out("User existe");
+                        $user = $gdata->retrieveUser($usernames[0]);
+                        $user->name->givenName = $u['Entidade']['nomes'];
+                        $user->name->familyName = $u['Entidade']['apelido'];
+                        $user->login->changePasswordAtNextLogin = true;
+                        $user = $user->save();
+                    }
+                } catch (Exception $e) {
+                    var_dump($usernames[0]);
+                    var_dump($u['Entidade']['nomes']);
+                    var_dump($u['Entidade']['apelido']);
+                    var_dump($e->getMessage());
+                    continue;
+                }
+
+                $user = $gdata->retrieveUser($usernames[0]);
+                if ($user) {
+                    $user = $user->save();
+                    if ($user != NULL) {
+                        $this->User->id = $u['User']['id'];
+                        $this->User->set('codigo_activacao', 1);
+                        if ($mudar_apelido) {
+                            $this->User->set('apelido', $u['Entidade']['apelido']);
+                        }
+                        $this->User->save();
+                    }
+                    $this->out($ii++);
+                    var_dump($user->login->userName);
+                }
+            }
+        }
+
+
+        public function init(){
+
+
+/**
+ * Client id and service account name as reported
+ * on https://console.developers.google.com/ - Projects - Credentials
+ */
+$client_id = '482133242525-6hnbvlngksqetc3ho7tfipapukkaccqf.apps.googleusercontent.com'; // 'CLIENT ID' on Google
+$service_account_name = '482133242525-6hnbvlngksqetc3ho7tfipapukkaccqf@developer.gserviceaccount.com'; // 'EMAIL ADDRESS' on Google
+
+/**
+ * This is the .p12 file generated on https://console.developers.google.com/ - Projects - Credentials
+ */
+$key_file_location = Configure::read('GOOGLEP12Location');
+
+/**
+ * Email address for admin user that should be used to perform API actions
+ * Needs to be created via Google Apps Admin interface and be added to an admin role
+ * that has permissions for Admin APIs for Users
+ */
+$delegatedAdmin = 'elisio.leonardo@uem.ac.mz';
+
+/**
+ * Array of scopes you need for whatever actions you want to perform
+ * See https://developers.google.com/admin-sdk/directory/v1/guides/authorizing
+ * The admin.directory.user is needed to create the user, the admin.directory.group is needed to add the
+ * user to a group (see later on this file)
+ */
+$scopes = array(
+    'https://www.googleapis.com/auth/admin.directory.user',
+    'https://www.googleapis.com/auth/admin.directory.group'
+);
+
+/**
+ * Create AssertionCredentails object for use with Google_Client
+ */
+$cred = new Google_Auth_AssertionCredentials(
+    $service_account_name,
+    $scopes,
+    file_get_contents($key_file_location)
+);
+/**
+ * API requests shall be used using the delegated admin
+ */
+//$cred->sub = $delegatedAdmin;
+            return $cred;
+        }
+
+        public function createUser($username,$firstName,$lastName,$password){
+            /**
+             * Create Google_Client for making API calls
+             */
+
+            $cred = $this->init();
+            $client = new Google_Client();
+            $client->setApplicationName("This is the name");
+            $cred->sub = 'elisio.leonardo@uem.ac.mz';
+            $client->setAssertionCredentials($cred);
+            if ($client->getAuth()->isAccessTokenExpired()) {
+                $client->getAuth()->refreshTokenWithAssertion($cred);
+            }
+            /**
+             * Create Google_Service_Directory
+             */
+            $service = new Google_Service_Directory($client);
+            /**
+             * Create the user
+             */
+            $nameInstance = new Google_Service_Directory_UserName();
+            $nameInstance -> setGivenName($firstName);
+            $nameInstance -> setFamilyName($lastName);
+            $email = $username;
+            $password = $password;
+            $userInstance = new Google_Service_Directory_User();
+            $userInstance -> setName($nameInstance);
+            $userInstance -> setHashFunction("MD5");
+            $userInstance -> setPrimaryEmail($email);
+            $userInstance -> setPassword(hash("md5", $password));
+            try
+            {
+                $createUserResult = $service->users->insert($userInstance);
+                return $createUserResult;
+            }
+            catch (Google_IO_Exception $gioe)
+            {
+                echo "Error in connection: ".$gioe->getMessage();
+                return false;
+            }
+            catch (Google_Service_Exception $gse)
+            {
+
+                if($gse->getCode()==409){
+                    return [false,['Email ja foi Criado']];
+                }
+                return false;
+            }
+        }
+    }
---
+++ app/Model/SmsNotification.php
@@ -0,0 +1,274 @@
+<?php
+
+App::uses('AppModel', 'Model');
+App::uses('SmsEnviada', 'Model');
+
+/**
+ * SmsNotification Model
+ *
+ * @property SmsNotificationType $SmsNotificationType
+ */
+class SmsNotification extends AppModel {
+//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	/**
+	 * belongsTo associations
+	 *
+	 * @var array
+	 */
+	public $belongsTo = array(
+		'SmsNotificationType' => array(
+			'className' => 'SmsNotificationType',
+			'foreignKey' => 'sms_notification_type_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+	public function processaSMSRenovacao($notification_id, $origin, $numero_estudante) {
+		App::uses('Aluno', 'Model');
+		$Aluno = new Aluno();
+		$Aluno->contain('Entidade');
+		$aluno = $Aluno->findByCodigo($numero_estudante);
+
+		$origin = substr($origin, 3);
+
+		$this->SmsEnviada = new SmsEnviada();
+		if (empty($aluno)) {
+			$message = "Numero de Estudante incorrecto";
+			$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, 0, 1, 0);
+			return $sms_enviada;
+		} else {
+//Vemos se o estudante eh regular
+			$is_regular = $Aluno->isRegular($aluno['Aluno']['id']);
+			if ($is_regular[0]['regular'] == true) {
+
+//Vemos se ja renovou 2014
+				$renovacao2014 = $Aluno->Matricula->getRenovacaoByAlunoAndAnoLectivo($aluno['Aluno']['id'], 2014);
+
+				if (!empty($renovacao2014)) {
+					$message = "A sua matricula ja foi renovada. Obrigado. www.dra.uem.mz.. twitter.com/uemdra";
+
+					$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+					return $sms_enviada;
+				} else {
+					$turno = $Aluno->Curso->getTurnoIdByCursoId($aluno['Aluno']['curso_id']);
+					if ($turno == 1) {
+						$valor_renovacao = 80;
+						$codigo_pagamento = "02";
+					} else {
+						$valor_renovacao = 160;
+						$codigo_pagamento = "03";
+					}
+
+					$ano_ingresso = $aluno['Aluno']['ano_ingresso'];
+					if ($ano_ingresso >= 2000 && $ano_ingresso <= 2007) {
+						$referencia = substr($aluno['Aluno']['codigo'], 2);
+					} elseif ($ano_ingresso > 2007) {
+						$referencia = "0" . $aluno['Aluno']['codigo'];
+					} else {
+						$referencia = $aluno['Aluno']['codigo'];
+					}
+					$message = "Status:Nao Renovada. Efectue Pagamento do Servico no  BIM. Entidade:77001, Referencia:" . $referencia . $codigo_pagamento . ", Valor:" . $valor_renovacao . "MT. Obrigado. www.dra.uem.mz";
+
+					$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+					return $sms_enviada;
+				}
+			} else {
+				$message = "A sua matricula nao e regular Por favor dirija-se com urgencia para a Direccao do Registo Academico. www.dra.uem.mz.. twitter.com/uemdra";
+				$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+				return $sms_enviada;
+			}
+		}
+
+		return true;
+	}
+
+	public function processaSMSConfirmacaoRenovacao($notification_id, $origin, $numero_estudante) {
+		App::uses('Aluno', 'Model');
+		$Aluno = new Aluno();
+		$Aluno->contain('Entidade');
+		$aluno = $Aluno->findByCodigo($numero_estudante);
+
+		$origin = substr($origin, 3);
+
+		$this->SmsEnviada = new SmsEnviada();
+		if (empty($aluno)) {
+			$message = "Numero de Estudante incorrecto";
+			$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, 0, 1, 0);
+			return $sms_enviada;
+		} else {
+//Vemos se o estudante eh regular
+			$is_regular = $Aluno->isRegular($aluno['Aluno']['id']);
+			if ($is_regular[0]['regular'] == true) {
+
+//Vemos se ja renovou 2014
+				$renovacao2014 = $Aluno->Matricula->getRenovacaoByAlunoAndAnoLectivo($aluno['Aluno']['id'], 2014);
+
+				if (!empty($renovacao2014)) {
+					$message = "A sua matricula ja foi renovada. Obrigado. www.dra.uem.mz.. twitter.com/uemdra";
+
+					$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+					return $sms_enviada;
+				} else {
+					$turno = $Aluno->Curso->getTurnoIdByCursoId($aluno['Aluno']['curso_id']);
+					if ($turno == 1) {
+						$valor_renovacao = 80;
+						$codigo_pagamento = "02";
+					} else {
+						$valor_renovacao = 160;
+						$codigo_pagamento = "03";
+					}
+
+					$ano_ingresso = $aluno['Aluno']['ano_ingresso'];
+					if ($ano_ingresso >= 2000 && $ano_ingresso <= 2007) {
+						$referencia = substr($aluno['Aluno']['codigo'], 2);
+					} else {
+						$referencia = "0" . $aluno['Aluno']['codigo'];
+					}
+					$message = "Renovacao de Matricula nao confirmada. Se fez o pagamento ha mais de 72h uteis, dirija-se ao Registo Academico para confirmacao manual.. Obrigado. UEM-DRA";
+
+					$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+					return $sms_enviada;
+				}
+			} else {
+				$message = "A sua matricula nao e regular Por favor dirija-se com urgencia para a Direccao do Registo Academico. www.dra.uem.mz.. twitter.com/uemdra";
+				$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+				return $sms_enviada;
+			}
+		}
+
+		return true;
+	}
+
+	public function getEmailEstudante($notification_id, $origin, $numero_estudante) {
+		App::uses('Aluno', 'Model');
+		$this->Aluno = new Aluno();
+		$this->Aluno->contain(array('Entidade' => array('User')));
+		$aluno = $this->Aluno->findByCodigo($numero_estudante);
+		$origin = substr($origin, 3);
+
+		$this->SmsEnviada = new SmsEnviada();
+		if (empty($aluno)) {
+			$message = "Numero de Estudante incorrecto";
+			$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, 0, 1, 0);
+			return $sms_enviada;
+		} else {
+			$is_regular = $this->Aluno->isRegular($aluno['Aluno']['id']);
+			if ($is_regular[0]['regular'] == true) {
+
+				$email_aluno = $aluno['Entidade']['User']['username'];
+				$nome_completo = $aluno['Entidade']['name'];
+				$message = "O Email Institucional de {$nome_completo} e:{$email_aluno}.";
+				$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+				return $sms_enviada;
+			} else {
+				$message = "A sua matricula nao e regular Por favor dirija-se com urgencia para a Direccao do Registo Academico. www.dra.uem.mz.. twitter.com/uemdra";
+				$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+				return $sms_enviada;
+			}
+		}
+	}
+
+	public function getTesteOJM($notification_id, $origin, $numero_estudante) {
+		App::uses('Aluno', 'Model');
+		$this->Aluno = new Aluno();
+		$this->Aluno->contain(array('Entidade' => array('User')));
+		$aluno = $this->Aluno->findByCodigo($numero_estudante);
+		$origin = substr($origin, 3);
+
+		$this->SmsEnviada = new SmsEnviada();
+		$message = "A sua quota a pagar eh de 10,00MT. Para pagar responda esta SMS com a palavra PAGAR";
+		$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+		return $sms_enviada;
+	}
+
+	public function getTesteOJM2($notification_id, $origin, $numero_estudante) {
+		App::uses('Aluno', 'Model');
+		$this->Aluno = new Aluno();
+		$this->Aluno->contain(array('Entidade' => array('User')));
+		$aluno = $this->Aluno->findByCodigo($numero_estudante);
+		$origin = substr($origin, 3);
+
+		$this->SmsEnviada = new SmsEnviada();
+		$message = "A sua quota esta liquidada. VOTE FILIPE NYUSI";
+		$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+		return $sms_enviada;
+	}
+
+	function randomPassword() {
+		$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+		$pass = array(); //remember to declare $pass as an array
+		$alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+		for ($i = 0; $i < 9; $i++) {
+			$n = rand(0, $alphaLength);
+			$pass[] = $alphabet[$n];
+		}
+		return implode($pass); //turn the array into a string
+	}
+
+	public function getSenhaEmailEstudante($notification_id, $origin, $numero_estudante) {
+		App::uses('Aluno', 'Model');
+		App::import('Vendor', 'zend_include_path');
+		App::import('Vendor', 'Zend_Loader', true, false, 'Zend/Loader.php');
+
+		Zend_Loader::loadClass('Zend_Gdata');
+		Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+		Zend_Loader::loadClass('Zend_Gdata_Gapps');
+
+		$service = Zend_Gdata_Gapps::AUTH_SERVICE_NAME;
+		$client = Zend_Gdata_ClientLogin::getHttpClient('elisio.leonardo@uem.ac.mz', 'COLOCAR SENHA AQUI', $service);
+		$gdata = new Zend_Gdata_Gapps($client, 'uem.ac.mz');
+		$this->Aluno = new Aluno();
+		$this->Aluno->contain(array('Entidade' => array('User')));
+		$aluno = $this->Aluno->findByCodigo($numero_estudante);
+		$origin = substr($origin, 3);
+
+		$this->SmsEnviada = new SmsEnviada();
+		if (empty($aluno)) {
+			$message = "Numero de Estudante incorrecto";
+			$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, 0, 1, 0);
+			return $sms_enviada;
+		} else {
+			$is_regular = $this->Aluno->isRegular($aluno['Aluno']['id']);
+			if ($is_regular[0]['regular'] == true) {
+
+				$email_aluno = $aluno['Entidade']['User']['username'];
+				$codigo_activacao = $aluno['Entidade']['User']['codigo_activacao'];
+				$random_password = $this->randomPassword();
+				if ($codigo_activacao != 2) {
+					$conta_existe = $gdata->retrieveUser($email_aluno);
+					if ($conta_existe) {
+						$conta_existe->login->password = $random_password;
+						$conta_existe->name->givenName = $u['Entidade']['nomes'];
+						$conta_existe->name->familyName = $u['Entidade']['apelido'];
+						$conta_existe->login->changePasswordAtNextLogin = true;
+						$conta_existe->save();
+					} else {
+						$novo_email = $gdata->createUser($email_aluno, $aluno['Entidade']['nomes'], $aluno['Entidade']['apelido'], $random_password);
+					}
+					$this->Aluno->Entidade->User->id = $aluno['Entidade']['User']['id'];
+					$this->Aluno->Entidade->User->set('codigo_activacao', 2);
+					$this->Aluno->Entidade->User->save();
+					$message = "O email {$email_aluno} foi activado. A senha inicial e:{$random_password}.";
+					$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+					return $sms_enviada;
+				} else {
+					$message = "Esta conta de email ja foi activada. Se tem certeza que a conta e sua e alguem activou por falha, ou perdeu a senha, contacte a DRA.";
+					$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+					return $sms_enviada;
+				}
+				$nome_completo = $aluno['Entidade']['name'];
+				$message = "O Email Institucional de {$nome_completo} e:{$email_aluno}.";
+				$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+				return $sms_enviada;
+			} else {
+				$message = "A sua matricula nao e regular Por favor dirija-se com urgencia para a Direccao do Registo Academico. www.dra.uem.mz.. twitter.com/uemdra";
+				$sms_enviada = $this->SmsEnviada->sendSMS($origin, $message, $aluno['Aluno']['entidade_id'], 1, 0);
+				return $sms_enviada;
+			}
+		}
+	}
+
+}
