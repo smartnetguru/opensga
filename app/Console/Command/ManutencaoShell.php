797c82ee3332b2551758c979e6a8defc4a7a0001
Elisio Leonardo <elisio.leonardo@gmail.com>
2016-01-16 09:27:33 +0200

    Organizando Composer

--- app/Console/Command/ManutencaoShell.php
+++ app/Console/Command/ManutencaoShell.php
@@ -9,7 +9,6 @@
      *
      * @property Aluno $Aluno
      */
-
     class ManutencaoShell extends AppShell
     {
 
@@ -36,8 +35,151 @@
         ];
 
 
-
         //Exportar dados actualizados para tabela alunos e ou entidades
+
+        public function actualiza_turmas_estudantes()
+        {
+
+            $this->Turma->contain('Curso');
+            $turmas = $this->Turma->find('all', ['conditions' => []]);
+            foreach ($turmas as $turma) {
+                $inscricaos = $this->Inscricao->find('count',
+                    ['conditions' => ['Inscricao.turma_id' => $turma['Turma']['id']]]);
+                $this->Turma->id = $turma['Turma']['id'];
+                $this->Turma->set('total_alunos', $inscricaos);
+                $this->Turma->save();
+                $this->out($turma['Turma']['name'] . '--------------------' . $inscricaos);
+
+            }
+        }
+
+        public function ajustaUserGroup()
+        {
+            $users = $this->User->find('all');
+            $total = count($users);
+            foreach ($users as $user) {
+                $this->out($total--);
+                //funcionario
+                $funcionario = $this->User->Entidade->Funcionario->findByUserId($user['User']['id']);
+                if (!empty($funcionario)) {
+                    $grupos['GroupsUser'] = [
+                        'user_id'  => $user['User']['id'],
+                        'group_id' => 2
+                    ];
+                    $grupoExiste = $this->User->GroupsUser->findByUserIdAndGroupId($user['User']['id'], 2);
+                    if (empty($grupoExiste)) {
+                        $this->out('Criando Funcionario');
+                        $this->User->GroupsUser->create();
+                        $this->User->GroupsUser->save($grupos);
+                    }
+
+                }
+                //docente
+                $entidade = $this->User->Entidade->findByUserId($user['User']['id']);
+                $docente = $this->User->Entidade->Docente->findByEntidadeId($entidade['Entidade']['id']);
+                if (!empty($docente)) {
+                    $grupos['GroupsUser'] = [
+                        'user_id'  => $user['User']['id'],
+                        'group_id' => 4
+                    ];
+                    $grupoExiste = $this->User->GroupsUser->findByUserIdAndGroupId($user['User']['id'], 4);
+                    if (empty($grupoExiste)) {
+                        $this->out('Criando Docente');
+                        $this->User->GroupsUser->create();
+                        $this->User->GroupsUser->save($grupos);
+                    }
+
+                }
+                //estudante
+                $aluno = $this->User->Entidade->Aluno->findByUserId($user['User']['id']);
+                if (!empty($aluno)) {
+                    $grupos['GroupsUser'] = [
+                        'user_id'  => $user['User']['id'],
+                        'group_id' => 2
+                    ];
+                    $grupoExiste = $this->User->GroupsUser->findByUserIdAndGroupId($user['User']['id'], 2);
+                    if (empty($grupoExiste)) {
+                        $this->out('Criando Aluno');
+                        $this->User->GroupsUser->create();
+                        $this->User->GroupsUser->save($grupos);
+                    }
+
+                }
+            }
+        }
+
+        public function ajusta_alunos_matriculas()
+        {
+            $alunos = $this->Aluno->find('all');
+            $total = count($alunos);
+            foreach ($alunos as $aluno) {
+                $this->Aluno->Matricula->contain('AnoLectivo');
+                $ultimaMatricula = $this->Aluno->Matricula->find('first', [
+                    'conditions' => [
+                        'Matricula.aluno_id'            => $aluno['Aluno']['id'],
+                        'Matricula.estado_matricula_id' => 1
+                    ],
+                    'order'      => 'AnoLectivo.ano DESC'
+                ]);
+                $anoFim = $ultimaMatricula['AnoLectivo']['ano'];
+                $anoInicio = $aluno['Aluno']['ano_ingresso'];
+
+                $anoLectivos = $this->Aluno->Matricula->AnoLectivo->find('list',
+                    ['conditions' => ['AnoLectivo.ano >' => $anoInicio, 'AnoLectivo.ano <' => $anoFim]]);
+                foreach ($anoLectivos as $k => $v) {
+                    $matriculaExiste = $this->Aluno->Matricula->findByAlunoIdAndAnoLectivoId($aluno['Aluno']['id'], $k);
+                    if (empty($matriculaExiste)) {
+                        $arrayMatricula = [
+                            'Matricula' => [
+                                'aluno_id'            => $aluno['Aluno']['id'],
+                                'curso_id'            => $aluno['Aluno']['curso_id'],
+                                'plano_estudo_id'     => $aluno['Aluno']['plano_estudo_id'],
+                                'data'                => $v . '-01-01',
+                                'estado_matricula_id' => 1,
+                                'user_id'             => 1,
+                                'ano_lectivo_id'      => $k,
+                                'tipo_matricula_id'   => 2,
+
+                            ]
+                        ];
+
+                        $this->Aluno->Matricula->create();
+                        if (!$this->Aluno->Matricula->save($arrayMatricula)) {
+                            die(debug($this->Aluno->Matricula->validationErrors));
+                        }
+                    }
+                }
+                $total--;
+                $this->out($total . '------------------------');
+            }
+        }
+        
+
+        public function apaga_docentes_duplicados()
+        {
+
+            $docentes = $this->Docente->find('all');
+            $i = 1;
+            foreach ($docentes as $docente) {
+                $i++;
+                $entidadeId = $docente['Docente']['entidade_id'];
+                $outrosDocentes = $this->Docente->find('all',
+                    ['conditions' => ['Docente.id NOT' => $docente['Docente']['id'], 'entidade_id' => $entidadeId]]);
+                if (empty($outrosDocentes)) {
+                    $this->out($i . '------------------Clean');
+
+                    continue;
+
+                } else {
+                    foreach ($outrosDocentes as $outro) {
+                        $this->Docente->delete($outro['Docente']['id']);
+                        $this->out($i . '------------------------' . $outro['Docente']['id']);
+                    }
+                }
+
+
+            }
+        }
 
         public function graduacao()
         {
@@ -47,7 +189,7 @@
                 throw new CakeException('Vendor class PHPExcel not found!');
             }
 
-            $xls = PHPExcel_IOFactory::load(APP . 'Imports' . DS .  'graduacao.xlsx');
+            $xls = PHPExcel_IOFactory::load(APP . 'Imports' . DS . 'graduacao.xlsx');
 
             $worksheet = $xls->getActiveSheet();
             $linha_actual = 2;
@@ -57,7 +199,7 @@
 
 
                 $codigo = $worksheet->getCell('A' . $linha_actual)->getCalculatedValue();
-                if($codigo==''){
+                if ($codigo == '') {
                     break;
                 }
 
@@ -67,30 +209,31 @@
 
 
                 $aluno = $this->Aluno->findByCodigo($codigo);
-                if(empty($aluno)){
+                if (empty($aluno)) {
                     continue;
                 }
-                $candidatoGraduacao = $this->Aluno->CandidatoGraduacao->findByAlunoIdAndCerimoniaGraduacaoId($aluno['Aluno']['id'],5);
-                if(!empty($candidatoGraduacao)){
+                $candidatoGraduacao = $this->Aluno->CandidatoGraduacao->findByAlunoIdAndCerimoniaGraduacaoId($aluno['Aluno']['id'],
+                    5);
+                if (!empty($candidatoGraduacao)) {
                     $arrayDados = [
-                        'CandidatoGraduacao'=>[
-                            'candidato_graduacao_id'=>$candidatoGraduacao['CandidatoGraduacao']['id'],
-                            'apelido'=>$nomes['surname'],
-                            'nomes'=>'firstname',
-                            'telemovel'=>$candidatoGraduacao['CandidatoGraduacao']['telemovel'],
-                            'data_defesa'=>$candidatoGraduacao['CandidatoGraduacao']['data_defesa'],
-                            'media_defesa'=>$candidatoGraduacao['CandidatoGraduacao']['media_defesa']
+                        'CandidatoGraduacao' => [
+                            'candidato_graduacao_id' => $candidatoGraduacao['CandidatoGraduacao']['id'],
+                            'apelido'                => $nomes['surname'],
+                            'nomes'                  => 'firstname',
+                            'telemovel'              => $candidatoGraduacao['CandidatoGraduacao']['telemovel'],
+                            'data_defesa'            => $candidatoGraduacao['CandidatoGraduacao']['data_defesa'],
+                            'media_defesa'           => $candidatoGraduacao['CandidatoGraduacao']['media_defesa']
                         ]
                     ];
 
-                    if(!$this->Aluno->CandidatoGraduacao->confirmaDados($arrayDados)){
+                    if (!$this->Aluno->CandidatoGraduacao->confirmaDados($arrayDados)) {
                         debug($arrayDados);
                         debug('falha');
                         die();
-                    } else{
-                        $this->out('Aluno Actualizado Graduacao---------------'.$codigo);
+                    } else {
+                        $this->out('Aluno Actualizado Graduacao---------------' . $codigo);
                     }
-                } else{
+                } else {
                     $dataConclusaoNivel = [
                         'HistoricoCurso' => [
                             'data_conclusao' => date('Y-m-d'),
@@ -110,7 +253,7 @@
                             ]
                         ]
                     ];
-                    if(!$this->Aluno->concluirNivel($dataConclusaoNivel)){
+                    if (!$this->Aluno->concluirNivel($dataConclusaoNivel)) {
                         debug($dataConclusaoNivel);
                         debug('falha 21');
                         die();
@@ -118,96 +261,17 @@
                 }
 
 
-
-
                 $linha_actual++;
             }
         }
 
-
-        public function apaga_docentes_duplicados(){
-
-            $docentes = $this->Docente->find('all');
-            $i=1;
-            foreach($docentes as $docente){
-                $i++;
-                $entidadeId = $docente['Docente']['entidade_id'];
-                $outrosDocentes = $this->Docente->find('all',['conditions'=>['Docente.id NOT'=>$docente['Docente']['id'],'entidade_id'=>$entidadeId]]);
-                if(empty($outrosDocentes)){
-                    $this->out($i.'------------------Clean');
-
-                    continue;
-
-                } else{
-                    foreach($outrosDocentes as $outro){
-                        $this->Docente->delete($outro['Docente']['id']);
-                        $this->out($i.'------------------------'.$outro['Docente']['id']);
-                    }
-                }
-
-
-            }
-        }
-
-        public function actualiza_turmas_estudantes(){
-
-            $this->Turma->contain('Curso');
-            $turmas = $this->Turma->find('all',['conditions'=>[]]);
-            foreach($turmas as $turma){
-                $inscricaos = $this->Inscricao->find('count',['conditions'=>['Inscricao.turma_id'=>$turma['Turma']['id']]]);
-                $this->Turma->id = $turma['Turma']['id'];
-                $this->Turma->set('total_alunos',$inscricaos);
-                $this->Turma->save();
-                $this->out($turma['Turma']['name'].'--------------------'.$inscricaos);
-
-            }
-        }
-
-       public function ajusta_alunos_matriculas(){
-           $alunos = $this->Aluno->find('all');
-           $total = count($alunos);
-           foreach($alunos as $aluno){
-               $this->Aluno->Matricula->contain('AnoLectivo');
-               $ultimaMatricula = $this->Aluno->Matricula->find('first',['conditions'=>['Matricula.aluno_id'=>$aluno['Aluno']['id'],'Matricula.estado_matricula_id'=>1],'order'=>'AnoLectivo.ano DESC']);
-              $anoFim = $ultimaMatricula['AnoLectivo']['ano'];
-               $anoInicio = $aluno['Aluno']['ano_ingresso'];
-
-               $anoLectivos = $this->Aluno->Matricula->AnoLectivo->find('list',['conditions'=>['AnoLectivo.ano >'=>$anoInicio,'AnoLectivo.ano <'=>$anoFim]]);
-               foreach($anoLectivos as $k=>$v){
-                   $matriculaExiste = $this->Aluno->Matricula->findByAlunoIdAndAnoLectivoId($aluno['Aluno']['id'],$k);
-                   if(empty($matriculaExiste)){
-                       $arrayMatricula = [
-                           'Matricula'=>[
-                               'aluno_id' => $aluno['Aluno']['id'],
-                               'curso_id' => $aluno['Aluno']['curso_id'],
-                               'plano_estudo_id' => $aluno['Aluno']['plano_estudo_id'],
-                               'data' => $v.'-01-01',
-                               'estado_matricula_id' => 1,
-                               'user_id' => 1,
-                               'ano_lectivo_id' => $k,
-                               'tipo_matricula_id' => 2,
-
-                           ]
-                       ];
-
-                       $this->Aluno->Matricula->create();
-                       if(!$this->Aluno->Matricula->save($arrayMatricula)){
-                           die(debug($this->Aluno->Matricula->validationErrors));
-                       }
-                   }
-               }
-               $total--;
-               $this->out($total.'------------------------');
-           }
-       }
-
-
-        public function removeInscricaoSemTurma(){
+        public function removeInscricaoSemTurma()
+        {
             $inscricaos = $this->Inscricao->find('all');
             $total = count($inscricaos);
-            foreach($inscricaos as $inscricao){
+            foreach ($inscricaos as $inscricao) {
                 $turma = $this->Turma->findById($inscricao['Inscricao']['turma_id']);
-                if(empty($turma)){
+                if (empty($turma)) {
                     $this->out('Removendo');
                     $this->Inscricao->id = $inscricao['Inscricao']['id'];
                     $this->Inscricao->delete($inscricao['Inscricao']['id']);
@@ -215,63 +279,6 @@
                 $this->out($total--);
             }
         }
-
-
-        public function ajustaUserGroup(){
-            $users = $this->User->find('all');
-            $total = count($users);
-            foreach($users as $user){
-                $this->out($total--);
-                //funcionario
-                $funcionario = $this->User->Entidade->Funcionario->findByUserId($user['User']['id']);
-                if(!empty($funcionario)){
-                    $grupos['GroupsUser'] = [
-                        'user_id'=>$user['User']['id'],
-                        'group_id'=>2
-                    ];
-                    $grupoExiste = $this->User->GroupsUser->findByUserIdAndGroupId($user['User']['id'],2);
-                    if(empty($grupoExiste)){
-                        $this->out('Criando Funcionario');
-                        $this->User->GroupsUser->create();
-                        $this->User->GroupsUser->save($grupos);
-                    }
-
-                }
-                //docente
-                $entidade = $this->User->Entidade->findByUserId($user['User']['id']);
-                $docente = $this->User->Entidade->Docente->findByEntidadeId($entidade['Entidade']['id']);
-                if(!empty($docente)){
-                    $grupos['GroupsUser'] = [
-                        'user_id'=>$user['User']['id'],
-                        'group_id'=>4
-                    ];
-                    $grupoExiste = $this->User->GroupsUser->findByUserIdAndGroupId($user['User']['id'],4);
-                    if(empty($grupoExiste)){
-                        $this->out('Criando Docente');
-                        $this->User->GroupsUser->create();
-                        $this->User->GroupsUser->save($grupos);
-                    }
-
-                }
-                //estudante
-                $aluno = $this->User->Entidade->Aluno->findByUserId($user['User']['id']);
-                if(!empty($aluno)){
-                    $grupos['GroupsUser'] = [
-                        'user_id'=>$user['User']['id'],
-                        'group_id'=>2
-                    ];
-                    $grupoExiste = $this->User->GroupsUser->findByUserIdAndGroupId($user['User']['id'],2);
-                    if(empty($grupoExiste)){
-                        $this->out('Criando Aluno');
-                        $this->User->GroupsUser->create();
-                        $this->User->GroupsUser->save($grupos);
-                    }
-
-                }
-            }
-        }
-
-
 
 
     }
